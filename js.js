35b865e6628e05ad885348f693a71811fd7c40e8
NicoGimenez <nicolas.gimenezlemos@gmail.com>
2018-10-01 18:25:23 -0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    
+    $('.menu-toggle').click(function(){
+        $('nav').toggleClass('active')
+    })
+    
+    $('#btnp1').click(function(){
+        $('#titp1').hide();
+        $('#btnp1').hide();
+    })
+
+});
